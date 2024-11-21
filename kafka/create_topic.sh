595ce1ee92f7a8d6bc9ca412138e0c486eb40b88
Mihailo Spasojevic <mihailo.spasojevic95@gmail.com>
2024-11-21 17:24:43 +0100

    added script for creating topics

--- kafka/create_topic.sh
+++ kafka/create_topic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KAFKA_BROKER="localhost:9092"
+KAFKA_BROKER="kafka:9093"
 
 kafka-topics.sh --create --topic order-created --partitions 3 --replication-factor 1 --bootstrap-server $KAFKA_BROKER
 kafka-topics.sh --describe --topic order-created --bootstrap-server $KAFKA_BROKER
